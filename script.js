f8b12f95097e3b52d9ff12b76606b60ea828ff63
bdbai <bdbaiapp@163.com>
2018-08-03 00:14:59 +0800

    Add order

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 ((window) => {
-  const $order = window.document.querySelector('.order button')
+  const document = window.document
+  const $order = document.querySelector('.order button')
+  const $colors = document.querySelectorAll('#model ul.colors li')
+  const $capacities = document.querySelectorAll('#model ul.capacities li')
+  const selection = {
+    color: null,
+    capacity: null
+  }
+  const resetChoice = (property, elements) => {
+    elements.forEach($el => {
+      $el.classList.remove('selected')
+    })
+    selection[property] = null
+  }
+  const applyChoice = (property, elements, getValueFromEl) => {
+    elements.forEach($el => {
+      $el.addEventListener('click', e => {
+        e.preventDefault()
+        resetChoice(property, elements)
+        e.srcElement.classList.add('selected')
+        selection[property] = getValueFromEl($el)
+      })
+    })
+  }
+  applyChoice('color', $colors, $el => $el.getAttribute('aria-label'))
+  applyChoice('capacity', $capacities, $el => $el.textContent)
   $order.addEventListener('click', e => {
-    alert('Your order is being proceeded.')
+    const { color, capacity } = selection
+    if (color === null) {
+      return alert('Please select the color you prefer.')
+    }
+    if (capacity === null) {
+      return alert('Please select the capacity that meets your requirement.')
+    }
+    alert(`You've ordered a USPC Solar Power Bank with color ${color} and capacity ${capacity}!`)
   })
 })(window)
